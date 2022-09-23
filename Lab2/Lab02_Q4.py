bbf3e0db4607128a394577f8af6bafef6231f2a6
Kelvin Leong <kelvin.leong@mail.utoronto.ca>
2022-09-23 06:22:12 -0400

    Finished and polish q4

--- Lab2/Lab02_Q4.py
+++ Lab2/Lab02_Q4.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 10:56:23 2022
 
+@Collaborator:  Kelvin Leong
+                Patrick Sandoval
+"""
+############################ HEADER ##########################################
+# This python code contains the explanation, codes and plots for calculating
+# numerical roundoff errors in the Q4 exercise
+##############################################################################
+# Import the necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
-
-def p(u):
-    return (1-u)**8
-
-def q(u):
-    return 1 - 8*u + 28*u**2 - 56*u**3 + 70*u**4 - 56*u**5 + 28*u**6 - 8*u**7 + u**8
+import MyFunctions as myf
 
 #%%
 ################################# Q4a #######################################
+# Pick 500 points in the range 0.98 < u < 1.02
 N = 500
 u_range = np.linspace(0.98, 1.02, N)
-p_vals = p(u_range)
-q_vals = q(u_range)
 
+# Calculate p(u) and q(u)
+p_vals = myf.p(u_range)
+q_vals = myf.q(u_range)
+
+# Plot p(u) and q(u) vs. u
 plt.figure(figsize=(8,8))
 plt.plot(u_range, p_vals, 'r.', label='p(u)')
 plt.plot(u_range, q_vals, 'k.', label='q(u)')
@@ -25,11 +34,14 @@ plt.ylabel("Polynomial numerical value", fontsize=16)
 plt.title("Comparison of numerical value between expanded and simplified \nform of polynomial", fontsize=14)
 plt.grid('on')
 plt.legend()
+plt.savefig("Q4a.png")
 
 #%%
 ################################# Q4b #######################################
+# Calculate the difference p(u)-q(u)
 diff_vals = p_vals - q_vals
 
+# Plot the difference p(u)-q(u)
 plt.figure(figsize=(8,8))
 plt.plot(u_range, diff_vals, ".", label='p(u)-q(u)')
 plt.xlabel("u value", fontsize=16)
@@ -37,48 +49,99 @@ plt.ylabel("p(u)-q(u) numerical value", fontsize=16)
 plt.title("Difference in numerical value between expanded and simplified \nform of polynomial", fontsize=14)
 plt.grid('on')
 plt.legend()
+plt.savefig("Q4b-Plot.png")
 
+# Plot the histogram of the difference p(u)-q(u) 
 bin_array = np.linspace(min(diff_vals), max(diff_vals), 20)
-
 plt.figure(figsize=(8,8), tight_layout=True)
 plt.hist(diff_vals, bin_array, align='left', label='Data')
 plt.xlabel("p(u)-q(u)",fontsize=16)
 plt.ylabel("Number of occurance",fontsize=16)
 plt.title("Distribution of numerical value of p(u)-q(u)",fontsize=16)
+plt.savefig("Q4b-Histogram.png")
 
+# Calculate the standard deviation of the histogram (distribution)
 std_dev = np.std(diff_vals, ddof=1)
 
-C = 1
 
-term_p = (1-u_range)**8
+# Calculate the roundoff error using equation 3 in the lab (C=1e-16)
+rdf_error = myf.q4b_roundoff_error(u_range)
+# Take the median of the roundoff errors over the u values near 1
+mean_rdf_error = np.mean(rdf_error)
 
-
-mean_squared_diff = (sum(diff_vals**2))/N**2
-error = C * np.sqrt(N) * np.sqrt(mean_squared_diff)
-
+# Comparison between staandard deviation of distribution and the roundoff error estimate
 print(f"Standard deviation of distribution: {std_dev: .3e}")
-print(f"Roundoff error estimate: {error: .3e}")
+print(f"Roundoff error estimate: {mean_rdf_error: .3e}")
 
 #%%
 ############################### Q4c #########################################
-u_range = np.arange(0.9800, 0.9990, 0.0001)
+# Pick points greater than 0.98 but less than 1.0, calculate p(u) and q(u)
+u_range = np.linspace(0.980, 0.985, 500)
+p_vals = myf.p(u_range)
+q_vals = myf.q(u_range)
 
-p_vals = p(u_range)
-q_vals = q(u_range)
+# Calculate and Plot this absolute error
 err = abs(p_vals-q_vals)/abs(p_vals)
 
 plt.figure(figsize=(8,8))
 plt.plot(u_range, err, ".")
 plt.xlabel("u value",fontsize=16)
-plt.ylabel("Fractional Error?",fontsize=16)
-plt.title("Fractional error", fontsize=16)
-plt.yscale('log')
+plt.ylabel("|p-q|/|p|",fontsize=16)
+plt.title("|p-q|/|p| for 0.980 < u < 0.985", fontsize=16)
+#plt.yscale('log')
 plt.grid('on')
-#diff_vals = p_vals - q_vals
-#std_dev = np.std(diff_vals, ddof=1)
-#mean_squared_diff = (sum(u_range**2))/N
-#error = C * np.sqrt(N) * np.sqrt(mean_squared_diff)
-#print(f"Standard deviation of distribution: {std_dev: .3e}")
-#print(f"Roundoff error estimate: {error: .3e}")
+plt.savefig("Q4c-abs.png")
 
+#%%
+u_range = np.arange(0.982, 0.984, 0.0001) #0.9831
+
+# Calculate fractional error of p(u)-q(u) and take the absolute
+fractional_error = myf.q4c_fractional_error(u_range)
+abs_fractional_error = abs(fractional_error)
+
+# Plot the absolute fractional error
+plt.figure(figsize=(8,8))
+plt.plot(u_range, abs_fractional_error*100, ".", markersize=10)
+plt.xlabel("u value",fontsize=16)
+plt.ylabel("Absolute fractional error (%)",fontsize=16)
+plt.title("Absolute fractional error for 0.982 < u < 0.984", fontsize=16)
+#plt.yscale('log')
+plt.ylim([0,500])
+plt.grid('on')
+plt.savefig("Q4c-frac-error.png")
+
+print(abs_fractional_error)
+print(f"Median of the absolute fractional error: {np.median(abs_fractional_error)*100:.2f} %")
+
+
+#%%
+############################## Q4d ##########################################
+
+# Pick 500 points in the range 0.98 < u < 1.02, calculate f(u), f(u)-1
+u_range = np.linspace(0.98, 1.02, 500)
+f_vals = myf.q4d_f(u_range)
+product_quotient_diff = f_vals - 1
+
+# Plotting numerical value of f(u)-1
+plt.figure(figsize=(8,8))
+plt.plot(u_range, product_quotient_diff, ".", label='f-1')
+plt.xlabel("u value", fontsize=16)
+plt.ylabel("f(u) - 1 = u^8/(u^4 * u^4) - 1", fontsize=16)
+plt.title("Numerical value for f(u)-1", fontsize=16)
+plt.grid('on')
+plt.legend()
+plt.savefig("Q4d.png")
+
+# Calculate the standard deviation of f(u)-1
+std_dev = np.std(product_quotient_diff, ddof=1)
+
+# Calculate roundoff error according to eq4.5 (ignoring sqrt(2))
+quotient_rdoff_error = u_range**8 / (u_range**4 * u_range**4)
+C = 1e-16
+roundOffError = np.mean(C * quotient_rdoff_error )
+
+print(f"Standard deviation of f(u) distribution: {std_dev}")
+print(f"Round off error of f(u) according to eq4.5: {roundOffError}")
+
+plt.show()
 
--- Lab2/MyFunctions.py
+++ Lab2/MyFunctions.py
@@ -1,10 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+############################ HEADER ##########################################
+# This python file contains our self-defined function used for Lab02
+##############################################################################
+
+import numpy as np
+
 def find_mean(data):
     '''Returns the mean of a data set'''
     import numpy as np
     return(np.sum(data)/len(data))
+
+# Define integrand in the integral in Q2 as a function
+def q2_f(x):
+    return 4/(1+x**2)
     
 # Calculate area under the curve using trapezoidal rules (eq. 5.3)
 def trapezoidal(f,a,b,N):
+    """
+    Parameters: f: [function(float): float] Integrand function
+                a: Lower bound of the integral
+                b: Upper bound of the integral
+                N: Number of slices
+
+    Returns: Trapezoidal approximation of the integral
+    """
     # Width of slice
     h = (b - a) / N
     
@@ -16,6 +37,14 @@ def trapezoidal(f,a,b,N):
 
 # Calculate area under the curve using simpson's rules (eq. 5.9)
 def simpsons(f,a,b,N):
+    """
+    Parameters: f: [function(float): float] Integrand function
+                a: Lower bound of the integral
+                b: Upper bound of the integral
+                N: Number of slices
+
+    Returns: Simpson's approximation of the integral
+    """
     # Width of slice
     h = (b - a) / N
     
@@ -29,3 +58,68 @@ def simpsons(f,a,b,N):
             even_sum += f(a + k*h)
     result = h/3 * (f(a) + f(b) + 4 * odd_sum + 2 * even_sum)
     return result
+
+
+# Question 4 functions
+# Polynomial function in q4 (simplified form)
+def p(u):
+    return (1-u)**8
+
+# Polynomial function in q4 (expanded form)
+def q(u):
+    return 1 - 8*u + 28*u**2 - 56*u**3 + 70*u**4 - 56*u**5 + 28*u**6 - 8*u**7 + u**8
+
+# Calculate the roundoff error of p(u)-q(u)
+def q4b_roundoff_error(u):
+    # p(u)-q(u) can be written as a sum of 10 terms, each of these 10 terms
+    # is power of (1-u) or of u
+    term_p = ((1-u)**8)
+    term_q0 = -1
+    term_q1 = (8*u)
+    term_q2 = (-28*u**2)
+    term_q3 = (56*u**3)
+    term_q4 = (-70*u**4)
+    term_q5 = (+56*u**5)
+    term_q6 = (-28*u**6)
+    term_q7 = (+8*u**7)
+    term_q8 = (-u**8)
+
+    # Take the square of each of these term and sum them over
+    sum_square_x = term_p**2 + term_q0**2 + term_q1**2 + term_q2**2 + term_q3**2 + term_q4**2 \
+                    +term_q5**2 +term_q6**2 +term_q7**2 + term_q8**2
+        
+    # Calculate the root mean square of these 10 terms
+    rms = np.sqrt(sum_square_x/10)
+    
+    C = 1e-16
+    return C * np.sqrt(10) * rms
+
+# Calculate the fractional error p(u)-q(u)
+def q4c_fractional_error(u):
+    # Same roundoff error calculation same as previous function
+    term_p = ((1-u)**8)
+    term_q0 = -1
+    term_q1 = (8*u)
+    term_q2 = (-28*u**2)
+    term_q3 = (56*u**3)
+    term_q4 = (-70*u**4)
+    term_q5 = (+56*u**5)
+    term_q6 = (-28*u**6)
+    term_q7 = (+8*u**7)
+    term_q8 = (-u**8)
+    sum_square_x = term_p**2 + term_q0**2 + term_q1**2 + term_q2**2 + term_q3**2 + term_q4**2 \
+                    +term_q5**2 +term_q6**2 +term_q7**2 + term_q8**2
+    rms = np.sqrt(sum_square_x/10)
+    C = 1e-16
+    rdf_error = C * np.sqrt(10) * rms
+
+    # Calculate the sum of all 10 terms
+    sum_x = term_p+term_q0+term_q1+term_q2+term_q3+term_q4+term_q5+term_q6+term_q7+term_q8
+    
+    # Calculate the fractional error according to eq(4)
+    fractional_error = rdf_error/sum_x
+    return fractional_error
+
+# Product and quotient function in q4d
+def q4d_f(u):
+    return u**8/((u**4) * (u**4))
