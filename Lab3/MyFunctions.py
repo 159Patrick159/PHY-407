fd4112556f41ec46fdcd19e450ed6b90ea159542
Patrick Sandoval <patricksandovalromero@gmail.com>
2022-09-28 09:59:11 -0400

    Initial commit for MyFunctions.py

---
+++ Lab3/MyFunctions.py
@@ -0,0 +1,13 @@
+# Contributors:
+# Patrick Sandoval
+# Kelvin Leong
+
+####################################### HEADER ###########################################
+# This python script holds all the functions for lab03 in the process of answering 
+# all the questions and subquestions.
+import numpy as np
+
+# Define error funtion for Q1
+def erf(x):
+    return(np.exp(-np.power(x,2)))
+
