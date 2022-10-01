deffff3afc0413045c4655f11d1e0cb6fe886676
Kelvin Leong <kelvin.leong@mail.utoronto.ca>
2022-10-01 17:12:16 -0400

    Finished main body code for Q1

---
+++ Lab4/Lab04_Q1.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Contributors:
+# Patrick Sandoval
+# Kelvin Leong
+
+################################ HEADER ######################################
+# This file contains the main body code, pseudocode, and plots for solving the
+# linear systems with different methods in Q1
+##############################################################################
+
+import numpy as np
+from numpy.random import rand 
+from SolveLinear import GaussElim, PartialPivot
+from numpy.linalg import solve
+import matplotlib.pyplot as plt
+from time import time
+
+############################### Q1a ##########################################
+# Please refer in the SolveLinear.py file for the implementation of the
+# PartialPivot() function, where the GaussElim() function incorperates it
+
+A = np.matrix([[2,1,4,1],[3,4,-1,-1],[1,-4,1,5],[2,-2,1,3]], float)
+v = np.array([-4,3,9,7],float)
+#A = np.matrix([[0,1,4,1],[3,4,-1,-1],[1,-4,1,5],[2,-2,1,3]], float)
+#v = np.array([-4,3,9,7],float)
+x = GaussElim(A,v)
+print("Solution: ",x)
+x = PartialPivot(A,v)
+print("Solution: ",x)
+
+#%%
+############################### Q1b ##########################################
+N_array = np.arange(5,251,1)
+
+time_GE = []
+time_PP = []
+time_LU = []
+err_GE = []
+err_PP = []
+err_LU = []
+
+for N in N_array:
+    A = rand(N,N)
+    v = rand(N)
+
+    # Timing how it takes to solve in Gaussian Elimination and corresponding error
+    start = time()
+    x = GaussElim(A,v)
+    end = time()
+    time_GE.append(end-start)
+    vsol = np.dot(A, x)
+    err = np.mean(abs(v-vsol))
+    err_GE.append(err)
+    
+    # Timing how it takes to solve in Partial Pivoting and corresponding error
+    start = time()
+    x = PartialPivot(A,v)
+    end = time()
+    time_PP.append(end-start)
+    vsol = np.dot(A, x)
+    err = np.mean(abs(v-vsol))
+    err_PP.append(err)
+    
+    # Timing how it takes to solve in Partial Pivoting and corresponding error
+    start = time()
+    x = solve(A,v)
+    end = time()
+    time_LU.append(end-start)
+    vsol = np.dot(A, x)
+    err = np.mean(abs(v-vsol))
+    err_LU.append(err)
+    
+#%%
+plt.figure(figsize=(8,8))
+plt.plot(N_array, time_GE, 'ko', markersize=5, label='GaussElim')
+plt.plot(N_array, time_PP, 'mo', markersize=5, label='PartialPivot')
+plt.plot(N_array, time_LU, 'ro', markersize=5, label='LU')
+plt.xlabel("N", fontsize=16)
+plt.ylabel("Time [s]", fontsize=16)
+plt.yscale('log')
+plt.grid('on')
+plt.legend()
+plt.title("Time to solve N size linear system for each methods", fontsize=16)
+plt.savefig('Q1_time.png')
+
+plt.figure(figsize=(8,8))
+plt.plot(N_array, err_GE, 'ko', markersize=5, label='GaussElim')
+plt.plot(N_array, err_PP, 'mo', markersize=5, label='PartialPivot')
+plt.plot(N_array, err_LU, 'ro', markersize=5, label='LU')
+plt.xlabel("N", fontsize=16)
+plt.ylabel("Error", fontsize=16)
+plt.yscale('log')
+plt.grid('on')
+plt.legend()
+plt.title("Error in solving N size linear system for each methods", fontsize=16)
+plt.savefig('Q1_error.png')
+
+    
---
+++ Lab4/SolveLinear.py
@@ -0,0 +1,88 @@
+# SolveLinear.py
+# Python module for PHY407
+# Paul Kushner, 2015-09-26
+# Modifications by Nicolas Grisouard, 2018-09-26
+# This module contains useful routines for solving linear systems of equations.
+# Based on gausselim.py from Newman
+#from numpy import empty
+# The following will be useful for partial pivoting
+from numpy import empty, copy, argmax
+
+
+def GaussElim(A_in, v_in):
+    """Implement Gaussian Elimination. This should be non-destructive for input
+    arrays, so we will copy A and v to
+    temporary variables
+    IN:
+    A_in, the matrix to pivot and triangularize
+    v_in, the RHS vector
+    OUT:
+    x, the vector solution of A_in x = v_in """
+    # copy A and v to temporary variables using copy command
+    A = copy(A_in)
+    v = copy(v_in)
+    N = len(v)
+    
+    for m in range(N):
+        # Divide by the diagonal element
+        div = A[m, m]
+        A[m, :] /= div
+        v[m] /= div
+
+        # Now subtract from the lower rows
+        for i in range(m+1, N):
+            mult = A[i, m]
+            A[i, :] -= mult*A[m, :]
+            v[i] -= mult*v[m]
+
+    # Backsubstitution
+    # create an array of the same type as the input array
+    x = empty(N, dtype=v.dtype)
+    for m in range(N-1, -1, -1):
+        x[m] = v[m]
+        for i in range(m+1, N):
+            x[m] -= A[m, i]*x[i]
+    return x
+
+
+def PartialPivot(A_in, v_in):
+    """ In this function, code the partial pivot (see Newman p. 222) """
+    # (Deep) copy A and v so the parameters remain unchanged
+    A = copy(A_in)
+    v = copy(v_in)
+    N = len(v)
+    #print("Original Matrix A: \n",A)
+    
+    for m in range(N):
+        # At the mth row, compare it to all lower rows, looking at the value 
+        # each row has in its mth element and finding the index whose value that is farthest from zero
+        max_idx = m + argmax(abs(A[m: ,m]))
+        
+        # Perform row swapping if current diagonal value is not already further from zero in mth column
+        if A[m,m] < A[max_idx,m]:
+            A[m,:], A[max_idx,:] = copy(A[max_idx,:]), copy(A[m,:])
+            v[m], v[max_idx] = copy(v[max_idx]), copy(v[m])
+            #print(f"Max index is {max_idx}. After row swapping:")
+            #print(A)
+            
+        # Divide by the diagonal element
+        div = A[m, m]
+        A[m, :] /= div
+        v[m] /= div
+    
+        # Now subtract from the lower rows
+        for i in range(m+1, N):
+            mult = A[i, m]
+            A[i, :] -= mult*A[m, :]
+            v[i] -= mult*v[m]
+    
+    #print("Resulting Matrix A from Gaussian Elimination with Partial Pivoting: \n", A)
+    
+    # Backsubstitution
+    # create an array of the same type as the input array
+    x = empty(N, dtype=v.dtype)
+    for m in range(N-1, -1, -1):
+        x[m] = v[m]
+        for i in range(m+1, N):
+            x[m] -= A[m, i]*x[i]
+    return x
