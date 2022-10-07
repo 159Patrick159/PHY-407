beff7e3390a402cffd57716604aa49d932c92e93
Kelvin Leong <kelvin.leong@mail.utoronto.ca>
2022-10-06 23:09:58 -0400

    Q3 pseudocode, function refractor, plot

--- Lab4/Lab04_Q3.py
+++ Lab4/Lab04_Q3.py
@@ -9,48 +9,38 @@
 # This file contains the main body code, pseudocode, and plots for solving the
 # non-linear systems with different methods in Q3
 ##############################################################################
-
+# See pseudo-code for each subsection
+# Import libraries and functions
 import numpy as np
 import matplotlib.pyplot as plt
-
+from MyFunctions import Q3ab_f, Q3ab_f_derivative, relaxation_method, overrelaxation_method, Q3c_f, Q3c_f_derivative, binary_search, Newton_method
+#%%
 ################################# Q3a ########################################
 # Exercise 6.10 (a, b)
+# Part a: Set c = 2, accuracy to 10^(-6)
+# Gives a initial guess of x
+# Write f(x) = 1 - e^{-cx} and its derivative as a function
+# Implement the relaxation method:
+    # While the error is greater than desired accuracy,
+    # Record the value of x_next = f(x_guess) and x_guess,
+    # Calculate the error using these two values and f'(x_next) according to Eq 6.83
+    # Repeat until error less than desired accuracy
+# Prints out the numerical solution x and its error
+# Part b: Initialze an array of c from 0 to 3 in steps of 0.01
+# Use relaxation method to store the numerical solution x at each c
+# Plots of x as a function of c
+    
 # Part a
 c = 2
 accuracy = 1e-6
 x_guess = 0.5
-error = 1.0
-
-print(f"Initial guess: {x_guess}")
-
-def Q3ab_f(x,c):
-    return 1 - np.exp(-c*x)
-
-def Q3ab_f_derivative(x,c):
-    return c*np.exp(-c*x)
-
-def relaxation_method(f, fprime, x, c, accuracy):
-    x1 = x
-    error = 1.0
-    counter = 0
-    # Loop until error is smaller than target accuracy
-    while error > accuracy:
-        x1, x2 = f(x1,c), x1
-        error = abs((x1 - x2)/(1- 1/fprime(x1,c)))
-        #print(x1, error)
-        counter += 1
-    return x1, error, counter
 
 x, error, _ = relaxation_method(Q3ab_f, Q3ab_f_derivative, x_guess, c, accuracy)
-#while error > accuracy:
-#    x1, x2 = 1 - np.exp(-c*x1), x1
-#    error = abs((x1 - x2)/(1- 1/(c*np.exp(-c*x2))))
-    #print(x1, error)
 
+print(f"Initial guess: {x_guess}")
 print(f"Numerical solution for x: {x:.6f} converge to a solution accurate to 1e-6")
 print(f"Error of the numerical solution: {error:.3e}")
 
-#%%
 # Part b
 c_array = np.arange(0.01, 3, 0.01)
 x_sol_array = []
@@ -58,15 +48,10 @@ x_sol_array = []
 for c in c_array:
     x_guess = 0.5
     x, error, _ = relaxation_method(Q3ab_f, Q3ab_f_derivative, x_guess, c, accuracy)
-    #error = 1.0
-    # Loop until error is smaller than target accuracy
-    #while error > accuracy:
-    #    x1, x2 = 1 - np.exp(-c*x1), x1
-    #    error = abs((x1 - x2)/(1- 1/(c*np.exp(-c*x2))))
-    #    #print(x1, error)
+
     x_sol_array.append(x)
     
-
+# Plotting x as a function of c
 plt.figure(figsize=(8,8))
 plt.plot(c_array, x_sol_array, 'ro')
 plt.xlabel("c", fontsize=16)
@@ -78,117 +63,76 @@ plt.savefig('Q3a.png')
 #%%
 ################################ Q3b #########################################
 # Exercise 6.11 (b,c,d)
+# Pseudo-code: 
+# Part b: Again set c = 2 and accuracy to 10^{-6}
+# Use the implemented method to printout the numerical solution x,
+# the error and this time also the counter for how many steps it takes to solve
+# Part c: Implement of the over-relaxation method similar to relaxation method 
+# but takes in omega as additional argument
+# Modify x_next = (1+omega)*f(x_guess) - omega*x
+# Modify error function as in part a
+# Starts with omega = 0.5, manually change omega until counter is minimized
+# Print out the numerical solution, error, and counter of steps
+    
 # Part b
 c = 2
 accuracy = 1e-6
 x_guess = 0.5
-#error = 1.0
-#steps = 0
+
+x, error, counter = relaxation_method(Q3ab_f, Q3ab_f_derivative, x_guess, c, accuracy)
 
 print(f"Initial guess: {x_guess}")
-x, error, counter = relaxation_method(Q3ab_f, Q3ab_f_derivative, x_guess, c, accuracy)
-#while error > accuracy:
-#    x1, x2 = 1 - np.exp(-c*x1), x1
-#    error = abs((x1 - x2)/(1- 1/(c*np.exp(-c*x2))))
-    #print(x1, error)
-#    steps += 1
 print(f"Numerical solution for x: {x:.6f}, converge to a solution accurate to 1e-6 after {counter} steps")
 print(f"Error of the numerical solution: {error:.3e}")
 
-#%%
+
 # Part c
 c = 2
 accuracy = 1e-6
 x_guess = 0.5
-#error = 1.0
 omega = 0.68
-#steps = 0
-
-def overrelaxation_method(f, fprime, x, c, accuracy, omega):
-    x1 = x
-    error = 1.0
-    counter = 0
-    # Loop until error is smaller than target accuracy
-    while error > accuracy:
-        x1, x2 = (1+omega)*f(x1,c) - omega*x1, x1
-        error = abs((x1 - x2)/(1- 1/((1+omega)*fprime(x1,c) - omega)))
-        #print(x1, error)
-        counter += 1
-    return x1, error, counter
 
 x, error, counter = overrelaxation_method(Q3ab_f, Q3ab_f_derivative, x_guess, c, accuracy, omega)
 
 print("Over-relaxation iteration")
 print(f"Initial guess: {x_guess}")
-#while error > accuracy:
-#    x1, x2 = (1+omega)*(1 - np.exp(-c*x1))-omega*x1, x1
-#    error = abs((x1 - x2)/(1- 1/((1+omega) * c*np.exp(-c*x2) - omega)))
-#    steps += 1
 print(f"Numerical solution for x: {x:.6f}, converge to a solution accurate to 1e-6 after {counter} steps")
 print(f"Error of the numerical solution: {error:.3e}")
 
 #%%
 ############################### Q3c ##########################################
 # Exercise 6.13
-# x is the solution to f(x) = 5e^{-x} + x - 5 = 0 
-# 
+# Set accuracy to 10^{-6}
+# Define function f(x) = 5e^{-x} + x - 5 (x such that f(x)=0 is a solution)
+# and calculate its derivative function f'(x)
+# Gives two initial guesses x1,x2 that bound the solution, check if 
+# f(x1) and f(x2) have opposite sign
+# Implement the binary search method (see MyFunctions for detail)
+# Implement the newton's method (see MyFunctions for detail)
+# Use binary search, newton's method and relaxation method with same initial guess to numerically solve
+# for x, and print out their answer and number of steps to compare
+# Use binary search's answer to calculate Wien's constant
+# Use Wien's constant and Sun's peak wavelength to find temperature
+
 accuracy = 1e-6
 
 # Binary search method (procedure in pg.264)
 x1 = 3.0
 x2 = 5.0
-
-def Q3c_f(x):
-    return 5*np.exp(-x) + x - 5
-
-def Q3c_f_derivative(x):
-    return -5*np.exp(-x) + 1
-
-# Check if f(x1) and f(x2) have opposite signs
 print(f"f(x1) and f(x2) has opposite sign: {Q3c_f(x1)*Q3c_f(x2) < 0}")
 
-# Use recursion to implement binary search
-def binary_search(f, x1, x2, accuracy):
-    global binary_counter
-    binary_counter += 1
-    
-    f_x1 = f(x1)
-    #f_x2 = f(x2)
-    x_half = 0.5 * (x1 + x2)
-    f_x_half = f(x_half)
-    
-    if (f_x1 * f_x_half > 0):
-        x1 = x_half
-    else:
-        x2 = x_half
-        
-    if(abs(x1 - x2) < accuracy):
-        return 0.5*(x1 + x2)
-    else:
-        return binary_search(f, x1, x2, accuracy)
-    
-binary_counter = 0
-binary_solution = binary_search(Q3c_f,x1,x2,accuracy)
+binary_solution, binary_counter = binary_search(Q3c_f,x1,x2,accuracy, 0)
 print(f"x solution from binaray search: {binary_solution:.7f}, in {binary_counter} steps")
 
 
+### Q3d ##########
 # Newton's method
-def Newton_method(f, fprime, x, accuracy):
-    counter = 0
-    delta = 1.0
-    while abs(delta) > accuracy:
-        delta = f(x)/fprime(x)
-        x -= delta
-        counter +=1
-    return x, counter
-
 newton_solution, newton_counter = Newton_method(Q3c_f, Q3c_f_derivative, x1, accuracy)
 print(f"x solution from Newton's method: {newton_solution:.7f}, in {newton_counter} steps")
 
 # Relaxation method
 error = 1.0
 counter = 0
-
 while error > accuracy:
     x1, x2 = 5*(1-np.exp(-x1)), x1
     error = abs((x1 - x2)/(1- 1/5*np.exp(-x1)))
@@ -197,6 +141,7 @@ while error > accuracy:
 
 relaxation_solution, relaxation_counter = x1, counter
 print(f"x solution from relaxation method: {relaxation_solution:.7f}, in {relaxation_counter} steps")
+###################
 
 
 # Calculating Wien's constant
--- Lab4/MyFunctions.py
+++ Lab4/MyFunctions.py
@@ -1,4 +1,13 @@
+# Contributors:
+# Patrick Sandoval
+# Kelvin Leong
+
+####################################### HEADER ###########################################
+# This python script holds the functions for Lab04 in the process of answering 
+# questions Q2 and Q3 and their subquestions.
+##########################################################################################
 import numpy as np
+
 def make_wf(EigV,x):
     '''Given the eigenvector it calculates
     the corresponding wavefunction by the superposition
@@ -8,3 +17,121 @@ def make_wf(EigV,x):
     for i in range(1,len(EigV)-1):
         tmp.append(EigV[i-1]*np.sin(i*np.pi*x/LA))
     return(sum(tmp))
+
+# Function for q3 a and b
+def Q3ab_f(x,c):
+    return 1 - np.exp(-c*x)
+
+# First derivative of function for q3 a and b
+def Q3ab_f_derivative(x,c):
+    return c*np.exp(-c*x)
+
+# Relaxation method that numerically search for solution of non-linear equations
+def relaxation_method(f, fprime, x, c, accuracy):
+    """
+    Parameters: f : function
+                fprime : 1st derivative of function
+                x : Initial guess of solution
+                c : A parameter for function f
+                accuracy : Desired accuracy of the numerical solution
+    Returns: x1 : The numerical solution with desired accuracy
+             error : Error on the numerical solution
+             counter : Number of steps takes to find x1
+    """
+    x1 = x
+    error = 1.0
+    counter = 0
+    # Loop until error is smaller than target accuracy
+    while error > accuracy:
+        x1, x2 = f(x1,c), x1
+        error = abs((x1 - x2)/(1- 1/fprime(x1,c)))
+        #print(x1, error)
+        counter += 1
+    return x1, error, counter
+
+# Overrelaxation method
+def overrelaxation_method(f, fprime, x, c, accuracy, omega):
+    """
+    Parameters: f : function
+                fprime : 1st derivative of function
+                x : Initial guess of solution
+                c : A parameter for function f
+                accuracy : Desired accuracy of the numerical solution
+                omega: Parameter for overrelaxation
+    Returns: x1 : The numerical solution with desired accuracy
+             error : Error on the numerical solution
+             counter : Number of steps takes to find x1
+    """
+    x1 = x
+    error = 1.0
+    counter = 0
+    # Loop until error is smaller than target accuracy
+    while error > accuracy:
+        x1, x2 = (1+omega)*f(x1,c) - omega*x1, x1
+        error = abs((x1 - x2)/(1- 1/((1+omega)*fprime(x1,c) - omega)))
+        #print(x1, error)
+        counter += 1
+    return x1, error, counter
+
+# Function for q3 c
+def Q3c_f(x):
+    return 5*np.exp(-x) + x - 5
+
+# First derivative of function for q3 c
+def Q3c_f_derivative(x):
+    return -5*np.exp(-x) + 1
+
+# Binary search method (procedure in pg.264)
+def binary_search(f, x1, x2, accuracy, binary_counter):
+    """
+    Parameters: f : function
+                x1 : Initial guess of lower bound of solution
+                x2 : Initial guess of upper bound of solution
+                accuracy : Desired accuracy of the numerical solution
+                binary_counter: Dummy variable used for return
+    Returns: Recursion! For numerical solution 
+             binary_counter : Number of steps takes to find solution / Depth 
+                              of recursion
+    """
+    # Use recursion to implement binary search
+    binary_counter += 1
+    
+    f_x1 = f(x1)
+    #f_x2 = f(x2)
+    
+    # Calculate the midpoint
+    x_half = 0.5 * (x1 + x2)
+    f_x_half = f(x_half)
+    
+    # Set x1 to midpoint if f(x_mid) has same sign as f(x_1)
+    # else set x2 to midpoint if f(x_mid) has same sign as f(x_2)
+    if (f_x1 * f_x_half > 0):
+        x1 = x_half
+    else:
+        x2 = x_half
+        
+    # Recursion step:
+    # If |x1-x2| less than target accuracy, take the midpoint as solution
+    # else calls binary_search()
+    if(abs(x1 - x2) < accuracy):
+        return 0.5*(x1 + x2), binary_counter    #Base case
+    else:
+        return binary_search(f, x1, x2, accuracy, binary_counter)
+    
+# Newton's method
+def Newton_method(f, fprime, x, accuracy):
+    """
+    Parameters: f : function
+                fprime : 1st derivative of function
+                x : Initial guess of solution
+                accuracy : Desired accuracy of the numerical solution
+    Returns: x: The numerical solution with desired accuracy
+             counter : Number of steps takes to find x
+    """
+    counter = 0
+    delta = 1.0
+    while abs(delta) > accuracy:
+        delta = f(x)/fprime(x)
+        x -= delta
+        counter +=1
+    return x, counter
